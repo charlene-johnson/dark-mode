3fc1db32656068e55d2e3267034c3cc77fb15877
charlene-johnson <charlene.j8234@gmail.com>
2020-06-09 22:47:22 -0400

    Finished localstorage componenet, trying to work on darkmode componenet

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,8 +2,17 @@ import {useState} from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
-       const item = window.localStorage.getItem(key);
-       return item ? JSON.parse(item) : initialValue;
+       if(window.localStorage.getItem(key)) {
+           return JSON.parse(window.localStorage.getItem(key));
+       }
+       window.localStorage.setItem(key, JSON.stringify(initialValue));
+       return initialValue;
     });
-    return [storedValue];
-}
+
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    };
+    return [storedValue, setValue];
+
+};
